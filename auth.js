355b858a5b4ead408c2042c4a315b2e6b2fc297b
Triston Tetley <tetleylax@aol.com>
2019-11-01 16:33:06 -0400

    added small portion of the authentication process to auth.js

--- auth.js
+++ auth.js
@@ -1,14 +1,29 @@
 //basic layout for ajax with spotify's examples
 //need to change redirect_URI after launch website
-URL = "https://accounts.spotify.com/authorize?client_id=603ba6b52bf24da0a23b5db603d64c25&redirect_uri=https://www.spotify.com/us/&response_type=token&scope=user-read-private%20user-read-email"
 
-$.ajax({
-    url: 'https://api.spotify.com/v1/me',
-    GET: URL
-    headers: {
-        'Authorization': 'Bearer ' + accessToken
-    },
-    success: function(response) {
-        ...
-    }
-   
+https://accounts.spotify.com/authorize?client_id=603ba6b52bf24da0a23b5db603d64c25&redirect_uri=http://localhost:3000/&scope=playlist-modify-public,playlist-modify-private,user-read-private,user-read-email&response_type=token
+
+$(document).ready(function () {
+    //console.log(window.location.hash);
+    function accessTokenCheckAndStore() {
+        const hashToken = window.location.hash;
+        if (hashToken) {
+            accessToken = hashToken.slice(1);
+            console.log(accessToken);
+        };
+    };
+
+    accessTokenCheckAndStore();
+
+     $.ajax({
+        url: 'https://api.spotify.com/v1/me',
+        headers: {
+            'Authorization': 'Bearer ' + accessToken
+        },
+        success: function (response) {
+            console.log(response);
+        }
+
+    });
+
+});
