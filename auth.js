48d2cff3c1fb5bdee48c994f747971d3ea9bd2a8
Adam Day <dayadamuga@gmail.com>
2019-11-03 17:25:52 -0500

    ready for glue, has add tracks and formatted JSON track quest add, and no bug in searchsong / create JSON request tracks

--- auth.js
+++ auth.js
@@ -115,33 +115,65 @@ $(document).ready(function () {
 
     function logSong() {
         console.log(typeof songsIdList);
-        console.log(songsIdListJSONObj);
+        console.log(songsList);
+        for (i=0; i<songsList.length; i++ ) {
+            track = encodeURIComponent(songsList[i].title);
+            artist = encodeURIComponent(songsList[i].artist);
+            searchSong(i).then(function (answer) {
+                if (answer.tracks.items.length != 0) {
+                    test.push(answer.tracks.items[0].id);
+                    console.info(test);
+                    console.log(test.length);
+                    //console.log(test[0]);
+                    //console.log(test[10]);
+                };
+            //test.push(songsList[i].title);
+        } );
+
     };
+};
 
     billboardSearch()
-        .then(createSongIdList)
-        .then(createPlaylistAddJSON)
+        //.then(postPlaylist)
+        //.then(createSongIdList)
+        //.then(createPlaylistAddJSON)
+        //.then(addTracks)
         .then(logSong);
 
     let track;
     let artist;
-    let songsIdList = [""];
-    console.log(typeof songsIdList);
+    //let songsIdList = [];
+    let test = [];
+    //console.log(typeof songsIdList);
+    console.log(Array.isArray(test));
 
-    //let songsIdList = ["7sO5G9EABYOXQKNPNiE9NR", "4PpuH4mxL0rD35mOWaLoKS", "3hBXvHLlTHvnbwrPbeoyAj", "0tYHqwTW4s6VuPWDSD7n7K", "6V4KHt9xu4TPEnDFoBeacT", "3T6MAweakkNrMYs8jZIWtg", "32lItqlMi4LBhb4k0BaSaC", "2yQZwi1P8AkkxxFhQ8rMEK", "609qKv3KPAbdtp0LQH2buA", "4EpLU30bi1KNWkBTfgZkoK", "54p39OJ544KF6iFeCzbVjs", "3wGXyJGsCf1myH5MooQIqE", "0SqmAwQrNyypiTroSwVjbX", "52ioLaTnO2J7BWY8UN2n9D", "7CEJH5Ed7FBbXF6CqBQPlJ", "5w3NgfV8v1nNZFLUBlGHIL", "2hl6q70unbviGo3g1R7uFx", "4vaxvNDaLSoD36iZX515ug", "6waqLPcT6ruMhmd36OhmFO", "0tgVpDi06FyKpA1z0VMD4v"];
+/*     for (i=0; i<songsList.length; i++ ) {
+        test.push(i);
+    }
+
+    console.log(test); */
+
+    let songsIdList = ["7sO5G9EABYOXQKNPNiE9NR", "4PpuH4mxL0rD35mOWaLoKS", "3hBXvHLlTHvnbwrPbeoyAj", "0tYHqwTW4s6VuPWDSD7n7K", "6V4KHt9xu4TPEnDFoBeacT", "3T6MAweakkNrMYs8jZIWtg", "32lItqlMi4LBhb4k0BaSaC", "2yQZwi1P8AkkxxFhQ8rMEK", "609qKv3KPAbdtp0LQH2buA", "4EpLU30bi1KNWkBTfgZkoK", "54p39OJ544KF6iFeCzbVjs", "3wGXyJGsCf1myH5MooQIqE", "0SqmAwQrNyypiTroSwVjbX", "52ioLaTnO2J7BWY8UN2n9D", "7CEJH5Ed7FBbXF6CqBQPlJ", "5w3NgfV8v1nNZFLUBlGHIL", "2hl6q70unbviGo3g1R7uFx", "4vaxvNDaLSoD36iZX515ug", "6waqLPcT6ruMhmd36OhmFO", "0tgVpDi06FyKpA1z0VMD4v"];
 
     function createSongIdList() {
-        songsList.forEach(function (element) {
+        for (i=0; i<songsList.length; i++ ) {
+            track = encodeURIComponent(songsList[i].title);
+            artist = encodeURIComponent(songsList[i].artist);
+            searchSong(i);
+        };
+/*         songsList.forEach(function (element) {
             track = encodeURIComponent(songsList[songsList.indexOf(element)].title);
             artist = encodeURIComponent(songsList[songsList.indexOf(element)].artist);
-            searchSong(songsList.indexOf(element)); //need to handle promise in order to get ordering correct?
-        });
+            let i = 0;
+            searchSong(i);
+            i++; //need to handle promise in order to get ordering correct?
+        }); */
         new Promise(function (resolve, reject) {
             resolve(songsIdList);
         });
     };
 
-    function searchSong(item) {
+    function searchSong(i) {
         return $.ajax({
             url: `https://api.spotify.com/v1/search?q=${track}+${artist}&type=track`,
             type: "GET",
@@ -150,40 +182,62 @@ $(document).ready(function () {
                 //console.log('Woooop! :)');
                 //console.log(response);
                 //console.log(response.tracks.items[0].id);
-                try {
-                    if (response.tracks.items[0] != undefined) {
-/*                         console.log(item);
-                        console.log(typeof songsIdList);
-                        console.log(typeof response.tracks.items[0].id); */
-                        songsIdList.push(response.tracks.items[0].id); // need to stringify? reults maybe key value pair
+                //try {
+                    if (response.tracks.items.length != 0) {
+                        //console.log(i);
+                        //console.log(typeof response.tracks.items[0].id)
+                        //test.push(response.tracks.items[0].id);
+                        //test.push(i);
+                        //console.log(Array.isArray(songsIdList)); // need to stringify? reults maybe key value pair
                     }; //only taking top search result
-                }
+/*                 }
                 catch (error) {
                     console.error(error);
-                }
+                } */
             },
             error: function () {
                 console.log();
                 console.error("errorrrrr");
             }
-        });
+        })
     };
 
-    let songsIdListJSON = [""];
+    let songsIdListJSON = [];
     let songsIdListJSONObj = {};
 
     function createPlaylistAddJSON() {
         console.log(songsIdList.length);
+        console.log(songsIdList);
         songsIdList.forEach(function (element) {
-            console.log("hi");
-            console.log(songsIdList);
-            console.log(element);
+            //console.log("hi");
+            //console.log(songsIdList);
+            //console.log(element);
             return songsIdListJSON.push("spotify:track:" + element);
         });
         songsIdListJSONObj = JSON.stringify({ "uris": songsIdListJSON });
         console.log(songsIdList);
         console.log(songsIdListJSON);
+        console.log(songsIdListJSONObj)
         return songsIdListJSONObj;
+    };
+
+    function addTracks() {
+        return $.ajax({
+            url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+            type: "POST",
+            data: songsIdListJSONObj,
+            dataType: 'json',
+            contentType: 'application/json',
+            beforeSend: function (xhr) { xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken.access_token); }, // in the accessToken object which is from cutting up the has response, access_token is the access token. 
+            success: function (response) {
+                console.log('Woo! :)');
+                console.log(response);
+            },
+            error: function () {
+                console.log();
+                console.error("error");
+            }
+        });
     };
 
 
